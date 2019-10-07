180e1e78fa27e22cedd05780443f5004af2351e9
David Ramirez <drami102@fiu.edu>
2019-10-07 16:23:12 -0400

    Part 2

---
+++ part2/README.md
@@ -0,0 +1,5 @@
+# Lab 2 Part 2
+
+To compile this program, run ```make``` or ```make all```
+
+To run this program, run ```./fib.out <integer value>```
---
+++ part2/fib.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int sum;
+int outArr[1024];
+void *runner(void *param);
+
+int main(int argc, char *argv[])
+{
+	pthread_t tid;
+	pthread_attr_t attr;
+	if (argc != 2)
+	{
+		fprintf(stderr,"usage: a.out <integer value>\n");
+		return -1;
+	}
+	if (atoi(argv[1]) < 0)
+    {
+        fprintf(stderr,"%d must be >= 0\n", atoi(argv[1]));
+        return -1;
+    }
+    pthread_attr_init(&attr);
+    pthread_create(&tid,&attr,runner,argv[1]);
+    pthread_join(tid, );
+    printf("sum = %d\n",sum);
+}
+
+void *runner(void *param)
+{
+    int i, upper = atoi(param);
+    outArr[0] = 0;
+    outArr[1] = 1;
+    for (i = 2; i <= upper; i++)
+        outArr[i] = outArr[i - 1] + outArr[i - 2];
+    pthread_exit(0);
+}
